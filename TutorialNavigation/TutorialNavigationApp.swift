c51b3c771bb05fe1433445cc5629e8b2ca114a57
Fernando de Lucas <54122934+FernandoDLucas@users.noreply.github.com>
2024-06-27 18:05:09 -0300

    Initial Commit

---
+++ TutorialNavigation/TutorialNavigationApp.swift
@@ -0,0 +1,17 @@
+//
+//  TutorialNavigationApp.swift
+//  TutorialNavigation
+//
+//  Created by Fernando de Lucas da Silva Gomes on 27/06/24.
+//
+
+import SwiftUI
+
+@main
+struct TutorialNavigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
